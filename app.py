da9c31d4e246679d2664cf0cc71958bd86830def
Ian Dai <ianbbdai@gmail.com>
2024-02-20 12:20:26 -0800

    Simple rendering of random cat

--- app.py
+++ app.py
@@ -1,7 +1,26 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
+key_header = {
+    "x-api-key": ""
+}
+api_url = "https://api.thecatapi.com/v1/images/search?has_breeds=1"
+
+
 @app.route("/")
-def hello_world():
-    return render_template('home.html')
+def home():
+    raw_resp = requests.get(
+        api_url,
+        headers=key_header,
+    )
+    resp = raw_resp.json()
+    cat_info = resp[0]["breeds"][0]
+
+    return render_template(
+        "home.html",
+        name=cat_info["name"],
+        description=cat_info["description"],
+        img_url=resp[0]["url"],
+    )
