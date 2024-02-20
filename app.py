20d0e8dc12c02d82fc0b3db86de96b8b0f5f5f62
Ian Dai <ianbbdai@gmail.com>
2024-02-20 15:58:59 -0800

    Added button to get more cat images

--- app.py
+++ app.py
@@ -4,13 +4,24 @@ import requests
 app = Flask(__name__)
 
 key_header = {
-    "x-api-key": ""
+    "x-api-key": "live_Qx0SvttrMJ6wrN7bQA1ujdumb11yNRV1ClWFXP4WQgH7W6pE35KXMZlval9jKfAi"
 }
 api_url = "https://api.thecatapi.com/v1/images/search?has_breeds=1"
 
 
 @app.route("/")
 def home():
+    cat_info = acquire_cat()
+
+    return render_template(
+        "home.html",
+        name=cat_info["name"],
+        description=cat_info["description"],
+        img_url=cat_info["img_url"],
+    )
+
+@app.route("/acquire-cat")
+def acquire_cat():
     raw_resp = requests.get(
         api_url,
         headers=key_header,
@@ -18,9 +29,8 @@ def home():
     resp = raw_resp.json()
     cat_info = resp[0]["breeds"][0]
 
-    return render_template(
-        "home.html",
-        name=cat_info["name"],
-        description=cat_info["description"],
-        img_url=resp[0]["url"],
-    )
+    return {
+        "name": cat_info["name"],
+        "description": cat_info["description"],
+        "img_url": resp[0]["url"],
+    }
